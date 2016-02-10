149bbabbb0c7e70edd10384c8e32780c0624dfd7
math <math@maths-mac.local>
2016-02-10 08:18:57 -0800

    Initial Commit

---
+++ Toolwatch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Toolwatch
+//
+//  Created by math on 2016-02-10.
+//  Copyright © 2016 Toolwatch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
