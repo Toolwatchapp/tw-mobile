0e8d0e350dacbfb1644ef56da46c3db8d82acf63
MathieuNls <mathieu.nayrolles@gmail.com>
2016-04-11 07:19:19 -0700

    register form API calls

--- Toolwatch/API.swift
+++ Toolwatch/API.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 import Alamofire
-
+	
 public class API{
     
     
--- Toolwatch/RegisterViewController.swift
+++ Toolwatch/RegisterViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class RegisterViewController: UITableViewWithHeader, UIPickerViewDataSource, UIPickerViewDelegate {
+class RegisterViewController: UITableViewWithHeader {
     
     @IBOutlet weak var email: UITextField!
     @IBOutlet weak var confirmEmail: UITextField!
@@ -18,8 +18,7 @@ class RegisterViewController: UITableViewWithHeader, UIPickerViewDataSource, UIP
     @IBOutlet weak var name: UITextField!
     @IBOutlet weak var firstname: UITextField!
     @IBOutlet weak var registerButton: UIButton!
-    @IBOutlet weak var country: UITextField!
-    var countries:[String]!
+    private var country:String = ""
     
     /**
      @IBOutlet weak var registerButton: UIButton!
@@ -33,14 +32,28 @@ class RegisterViewController: UITableViewWithHeader, UIPickerViewDataSource, UIP
         
         registerButton.layer.cornerRadius = 5;
         
-        self.countries = NSLocale.ISOCountryCodes().map { (code:String) -> String in
-            let id = NSLocale.localeIdentifierFromComponents([NSLocaleCountryCode: code])
-            return NSLocale(localeIdentifier: "en_US").displayNameForKey(NSLocaleIdentifier, value: id) ?? "Country not found for code: \(code)"
+        //http://stackoverflow.com/a/27491026
+        let locale: NSLocale = NSLocale(localeIdentifier: NSLocale.currentLocale().localeIdentifier)
+        let countryCode = locale.objectForKey(NSLocaleCountryCode) as! String
+        let country: String? = locale.displayNameForKey(NSLocaleCountryCode, value: countryCode)
+        
+        // According to the docs, "Not all locale property keys
+        // have values with display name values" (thus why the
+        // "country" variable's an optional). But if this one
+        // does have a display name value, you can print it like so.
+        if let foundCountry = country {
+            self.country = foundCountry;
+            print(self.country);
         }
         
-        let pickerView = UIPickerView()
-        pickerView.delegate = self
-        country.inputView = pickerView
+        //Set default values for optional variables
+//        email.text = "";
+//        confirmEmail.text = "";
+//        password.text = "";
+//        confirmPassword.text = "";
+//        name.text = "";
+//        firstname.text = "";
+        
     }
     
     @IBAction func register(sender: AnyObject) {
@@ -48,14 +61,15 @@ class RegisterViewController: UITableViewWithHeader, UIPickerViewDataSource, UIP
         if(email.text != confirmEmail.text || isValidEmail(email.text!) == false || password.text != confirmPassword.text || password.text?.characters.count < 6){
             let alertController = UIAlertController(
                 title: "Whoops!",
-                message:"Email must be valid and password at least 6 characters long",
+                message:"Email must be valid and password be at least 6 characters long",
                 preferredStyle: UIAlertControllerStyle.Alert
             );
             alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.Default,handler: nil));
             self.presentViewController(alertController, animated: true, completion: nil);
             
         }else{
-            API.registerEmail(email.text!, password: password.text!, name: name.text!, firstName: firstname.text!, country: country.text!, callback: {
+            
+            API.registerEmail(email.text!, password: password.text!, name: name.text!, firstName: firstname.text!, country: country, callback: {
                 
                 (result:Bool) in
                 if(result){
@@ -74,6 +88,8 @@ class RegisterViewController: UITableViewWithHeader, UIPickerViewDataSource, UIP
                     self.presentViewController(alertController, animated: true, completion: nil);
                 }
             });
+            
+            self.activityIndicator.startAnimating()
         }
     }
     
@@ -102,40 +118,18 @@ class RegisterViewController: UITableViewWithHeader, UIPickerViewDataSource, UIP
      */
     override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat
     {
-        //We used 30% of the frame for the header
-        //We need 10% for the validate button
-        //The 60% remaining are 40% for inputs and 20 for labels
-        
         //Even number are labels
-        if(indexPath.item % 2 == 0 && indexPath.item != 10){
-            //The heigh of the screen * (20% / 5 labels / 100)
+        if(indexPath.item % 2 == 0 && indexPath.item != 13){
             return self.view.frame.size.height * (20/5/100)
             
             //Odd numbers are input
-        }else if(indexPath.item != 10){
-            //The heigh of the screen * (40% / 5 inputs / 100)
+        }else if(indexPath.item != 13){
             return self.view.frame.size.height * (40/5/100)
             
             //The validate button
         }else{
-            return self.view.frame.size.height * 0.1
+            return self.view.frame.size.height * 0.2
         }
-    }
-    
-    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
-        return 1
-    }
-    
-    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return countries.count
-    }
-    
-    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return countries[row]
-    }
-    
-    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        country.text = countries[row]
     }
     
     /**
