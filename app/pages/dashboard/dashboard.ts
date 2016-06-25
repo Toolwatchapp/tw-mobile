78eedd117af50d5ccabb71ed4cfe6d9d22ab4b10
Mathieu Nayrolles <mathieu.nayrolles@gmail.com>
2016-06-25 17:21:22 -0400

    [WIP] Dashboard conditional watch status

--- app/pages/dashboard/dashboard.ts
+++ app/pages/dashboard/dashboard.ts
@@ -1,13 +1,18 @@
 import {Component, ElementRef, ViewChild} from '@angular/core';
-import {Alert, Nav, Loading, NavController} from 'ionic-angular';
+import {Alert, Nav, Loading, NavController, ActionSheet} from 'ionic-angular';
 import {LoginComponent} from 'tw-common/dist/app/directives/login/login.component';
 import {TwAPIService} from 'tw-common/dist/app/services/twapi.service';
-import {Watch} from 'tw-common/dist/app/models/watch.model';
+import {Watch, WatchStatus, WatchAction} from 'tw-common/dist/app/models/watch.model';
+import {MeasureStatus} from 'tw-common/dist/app/models/measure.model';
 import {User} from 'tw-common/dist/app/models/user.model';
 import {TRANSLATE_PROVIDERS, TranslateService, TranslatePipe, TranslateLoader, TranslateStaticLoader} from 'ng2-translate/ng2-translate';
 import {Http, HTTP_PROVIDERS, Headers}  from '@angular/http';
 import {ListPage} from '../list/list';
 import {Footer} from '../footer/footer';
+import {WatchPage} from '../watch/watch';
+import {Header} from '../header/header';
+import {MeasurePage} from '../measure/measure';
+
 import 'gsap';
 
 
@@ -15,30 +20,79 @@ import 'gsap';
 	templateUrl: 'build/pages/dashboard/dashboard.html',
 	pipes: [TranslatePipe],
 	providers: [TwAPIService, HTTP_PROVIDERS],
-	directives: [Footer]
+	directives: [Footer, Header]
 })
 export class DashboardPage {
 
 
 	user:User;
+	WatchStatus = WatchStatus;
+	MeasureStatus = MeasureStatus;
+	WatchAction = WatchAction;
 	
 
 	constructor(private nav: NavController, private translate: TranslateService,
 		private twapi: TwAPIService, private elementRef: ElementRef) {
+
+		var userLang = navigator.language.split('-')[0];
+		userLang = /(fr|en)/gi.test(userLang) ? userLang : 'en';
+		translate.setDefaultLang('en');
+		translate.use(userLang);
 
 		this.twapi.login("mathieu.nayrolles@gmail.com", "qwerty").then(
 			res => {
 				this.user = res;
 				console.log(this.user);
 
-				for (var i = 0; i < 20; i++) {
-					this.user.watches.push(
-						new Watch(0, "Jae")
-					)
+				for (var i = 0; i < this.user.watches.length; i++) {
+					console.log(this.user.watches[i]);
 				}
 			}  
 		);
+	}
 
+	updateWatch(watch:Watch){
+		this.nav.push(WatchPage, {
+			watch: watch,
+			user: this.user
+		});
+	}
+
+	measureWatch(watch: Watch){
+
+		this.nav.push(MeasurePage, {
+			watch: watch,
+			user: this.user
+		});
+
+	}
+
+	share(){
+
+		let actionSheet = ActionSheet.create({
+			title: 'Modify your album',
+			buttons: [
+				{
+					text: 'Destructive',
+					role: 'destructive',
+					handler: () => {
+						console.log('Destructive clicked');
+					}
+				}, {
+					text: 'Archive',
+					handler: () => {
+						console.log('Archive clicked');
+					}
+				}, {
+					text: 'Cancel',
+					role: 'cancel',
+					handler: () => {
+						console.log('Cancel clicked');
+					}
+				}
+			]
+		});
+		this.nav.present(actionSheet);
 		
 	}
 
@@ -47,7 +101,7 @@ export class DashboardPage {
 
 		let alert = Alert.create({
 			title: this.translate.instant('delete-watch-alert'),
-			message: this.translate.instant('delete-watch') + watch.brand + " " + watch.name + "?",
+			message: this.translate.instant('delete-watch-confirm') + watch.brand + " " + watch.name + "?",
 			buttons: [
 				{
 					text: this.translate.instant('cancel'),
