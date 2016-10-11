8bb6868ded40d5ce2383eacbe54269f6cc6e5381
Mathieu Nayrolles <mathieu.nayrolles@gmail.com>
2016-10-11 10:50:17 -0400

    update autologin for rc0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
-import { StatusBar } from 'ionic-native';
+import { StatusBar, NativeStorage } from 'ionic-native';
 
 import { LogInPage } from '../pages/login/login';
+import { DashboardPage } from '../pages/dashboard/dashboard';
+
+import {
+	TwAPIService
+} from './../share/src/app/';
 
 @Component({
   template: `<ion-nav [root]="rootPage"></ion-nav>`
@@ -12,9 +17,64 @@ export class MyApp {
 
   constructor(platform: Platform) {
     platform.ready().then(() => {
+
+      this.fetchUser()
+      .then(
+        user => {
+          if(user !== undefined){
+
+            console.error(user);
+            this.rootPage = DashboardPage;
+            setTimeout(()=> DashboardPage.userChanged.emit(user), 1000);
+          }else{
+            this.rootPage = LogInPage;
+          }
+        }
+      ).catch((error)=>{this.rootPage = LogInPage; console.log(error)})
+
+
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
     });
+  }
+
+  private initOnResume(){
+    document.addEventListener('resume', () => {
+
+      this.fetchUser().then(
+        user => {
+          DashboardPage.userChanged.emit(user)
+        }
+      );
+      
+      TwAPIService.resetTime();
+    });
+  }
+
+   private fetchUser():Promise<User>{
+
+    return this.fetchAPIKey().then(
+       key => {
+         if(key !== "cordova_not_available"){
+           return this.twapi.getUser(key).then(
+            user => user,
+            err => {console.log("removing", key); NativeStorage.remove('tw-api')}
+          )
+         }else{
+           console.log("removing", key); 
+           NativeStorage.remove('tw-api')
+           return undefined;
+         } 
+       }
+    );
+  }
+
+  private fetchAPIKey():Promise<string>{
+    return NativeStorage.getItem('tw-api')
+      .then(
+        data => data.key,
+        error => error
+      );
   }
 }
