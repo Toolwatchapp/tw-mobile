af43c9ab801bdc9f9bcab2ff11d225510cfe0cff
Mathieu Nayrolles <mathieu.nayrolles@gmail.com>
2016-11-02 09:44:49 -0400

    Undo prod fix. Build is now --dev anyway

--- src/app/main.prod.ts
+++ src/app/main.prod.ts
@@ -1,12 +1,7 @@
-// import { platformBrowser } from '@angular/platform-browser';
-// import { enableProdMode } from '@angular/core';
+import { platformBrowser } from '@angular/platform-browser';
+import { enableProdMode } from '@angular/core';
 
-// import { AppModuleNgFactory } from './app.module.ngfactory';
+import { AppModuleNgFactory } from './app.module.ngfactory';
 
-// enableProdMode();
-// platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app.module';
-
-platformBrowserDynamic().bootstrapModule(AppModule);
+enableProdMode();
+platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
